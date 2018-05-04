d23e6773b522484aa291017a9e13ccefb8cd7b42
Gregor Wolf <gregor.wolf@gmail.com>
2018-05-04 12:42:04 +0200

    Check NEO Tools extraction

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ cd ${WORKSPACE}/tmp/neo-java-web-sdk
 wget -nv 'http://central.maven.org/maven2/com/sap/cloud/neo-java-web-sdk/3.52.15/neo-java-web-sdk-3.52.15.zip'
 unzip -qq -o neo-java-web-sdk-3.52.15.zip
 rm neo-java-web-sdk-3.52.15.zip
+ls -la
+ls -la tools
 
 # extract artifact name
 cd ~/repo
