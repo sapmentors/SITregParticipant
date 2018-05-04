106b6797fec4a6b06aacea69a158d5c0a376756b
Gregor Wolf <gregor.wolf@gmail.com>
2018-05-04 12:48:45 +0200

    Try with path

--- build.sh
+++ build.sh
@@ -9,8 +9,7 @@ cd ${WORKSPACE}/tmp/neo-java-web-sdk
 wget -nv 'http://central.maven.org/maven2/com/sap/cloud/neo-java-web-sdk/3.52.15/neo-java-web-sdk-3.52.15.zip'
 unzip -qq -o neo-java-web-sdk-3.52.15.zip
 rm neo-java-web-sdk-3.52.15.zip
-ls -la
-ls -la tools
+export PATH=$PATH:~/repo/tmp/neo-java-web-sdk/tools/
 
 # extract artifact name
 cd ~/repo
@@ -25,4 +24,4 @@ sed -ie "s/\${timestamp}/`date +%Y%m%d%H%M%S`/g" mta.yaml
 java -jar ${WORKSPACE}/tmp/mta/mta.jar --mtar ${mtaName}.mtar --build-target=NEO build
 
 # deploy to SAP Cloud Platform
-${WORKSPACE}/tmp/neo-java-web-sdk/tools/neo.sh deploy-mta --user ${CI_DEPLOY_USER} --host ${DEPLOY_HOST} --source ${mtaName}.mtar --account ${CI_DEPLOY_ACCOUNT} --password ${CI_DEPLOY_PASSWORD} --synchronous
+neo.sh deploy-mta --user ${CI_DEPLOY_USER} --host ${DEPLOY_HOST} --source ${mtaName}.mtar --account ${CI_DEPLOY_ACCOUNT} --password ${CI_DEPLOY_PASSWORD} --synchronous
