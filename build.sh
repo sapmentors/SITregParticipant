b35a174a1aa2001dd92f00b421f6bd2ec1d5b0ea
Gregor Wolf <gregor.wolf@gmail.com>
2018-05-04 13:06:12 +0200

    Debug output

--- build.sh
+++ build.sh
@@ -22,6 +22,9 @@ wget -nv 'http://central.maven.org/maven2/com/sap/cloud/neo-java-web-sdk/3.52.15
 unzip -qq -o neo-java-web-sdk-3.52.15.zip
 rm neo-java-web-sdk-3.52.15.zip
 cd ~/repo
+ls -la tmp
+ls -la tmp/neo-java-web-sdk
+ls -la tmp/neo-java-web-sdk/tools
 cat tmp/neo-java-web-sdk/tools/neo.sh
 # deploy to SAP Cloud Platform
 bash -ex tmp/neo-java-web-sdk/tools/neo.sh deploy-mta --user ${CI_DEPLOY_USER} --host ${DEPLOY_HOST} --source ${mtaName}.mtar --account ${CI_DEPLOY_ACCOUNT} --password ${CI_DEPLOY_PASSWORD} --synchronous
